d84bdc5eee6dfc98f36819d62098999a76578643
Bakirovildar <bakirovildar2011@mail.ru>
2022-06-21 15:31:24 +0500

    Удалил лишний компонент

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <HeaderVue/>
   <router-view></router-view>
 </template>
 
 <script>
-import HeaderVue from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderVue
   }
 }
 </script>
